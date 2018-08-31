c958740a805c1e38b012645ae3b6e45de6293b3d
slim <smedini@gmail.com>
2018-08-31 17:56:54 +0200

    gateway auth!

--- piles/blueforest/files/etc/nginx/blueforest.org/api/lua/gateway.lua
+++ piles/blueforest/files/etc/nginx/blueforest.org/api/lua/gateway.lua
@@ -1,19 +1,27 @@
 local jwt = require "resty.jwt"
 local jwt_secret = os.getenv('JWT_SECRET')
-local gateway = {}
+local _M = {}
 
 assert(jwt_secret ~= nil, "Environment variable JWT_SECRET not set")
 
-function gateway.verifyToken()
-    local jwt_token = ngx.var.http_x_access_token
-    if jwt_token == nil then
-        ngx.exit(ngx.HTTP_UNAUTHORIZED)
-    else
-        assert(jwt_secret ~= nil, "Environment variable JWT_SECRET not set")
-        if not jwt:verify(jwt_secret, jwt_token).verified then
-            ngx.exit(ngx.HTTP_FORBIDDEN)
+function _M.verifyToken(x,keys)
+
+    local key = ngx.req.get_method() .. ngx.var.request_uri
+
+    for k,v in pairs(keys) do
+        if string.match(key,v) ~= nil then
+            local jwt_token = ngx.var.http_x_access_token
+            if jwt_token == nil then
+                ngx.exit(ngx.HTTP_UNAUTHORIZED)
+            else
+                if not jwt:verify(jwt_secret, jwt_token).verified then
+                    ngx.exit(ngx.HTTP_FORBIDDEN)
+                end
+            end
+            break
         end
     end
+
 end
 
-return gateway
+return _M
