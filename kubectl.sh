f9bd348da90e83909121d489823d4c97e8bd6dfa
slim <smedini@gmail.com>
2019-05-09 12:33:43 +0200

    conf

--- kubectl.sh
+++ kubectl.sh
@@ -8,3 +8,7 @@
 #reload gateway
 curl https://gist.githubusercontent.com/mnadel/4ddcc0ba07d398b35dad89b4dfdc8308/raw/f2b27f93c17bc4212c0f72174fd871693aeb1661/kcdo > run.sh
 sh run.sh -s gateway -n bf nginx -s reload
+
+
+#who primary?
+for ((i = 0; i < 3; ++i)); do kubectl exec --namespace mongodb-replicaset mongodb-replicaset-$i -- sh -c 'mongo --eval="printjson(rs.isMaster())"'; done
